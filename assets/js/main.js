f7d187867c953b1a68b721ed5dbdbdcec755049c
maksimpro001 <ytmaksik425@gmail.com>
2023-02-13 12:22:24 +0200

    JS Code

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+let num1 = prompt("Введіть перше число");
+let num2 = prompt("Введіть друге число");
+let sum = Number(num1) + Number(num2);
+
+if (sum > 10) {
+  console.log("ОК");
+}
+
+let name = prompt("Please enter your name");
+
+if (name == "Andrew") {
+  console.log("ПРИВІТ");
+} else {
+  console.log("ХТО ТИ?");
+}
